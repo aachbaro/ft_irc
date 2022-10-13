5f7ac443168813778784b45f240dc23c65c3a3df
aachbaro <aachbaro@student.42.fr>
2022-10-13 14:26:34 +0200

    woohoo

---
+++ Makefile
@@ -0,0 +1,19 @@
+NAME		= ft_irc
+SRCSDIR		= .
+OBJSDIR		= objs
+INCLUDES	= .
+SRCS		= srcs/main.cpp \
+				srcs/Server.cpp \
+				srcs/Client.cpp
+CC			= c++
+CFLAGS		= #-Wall -Wextra -Werror -std=c++98
+
+all: $(NAME)
+$(NAME): $(SRCSDIR)/$(SRCS)
+	@echo "Assembling $@"
+	@$(CC) $(CFLAGS) -o $@ $^
+clean:
+fclean: clean
+	rm -rf $(NAME)
+re: fclean all
+.PHONY: all clean fclean re test
---
+++ inc/Server.hpp
@@ -0,0 +1,57 @@
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netdb.h>
+# include <poll.h>
+# include <iostream>
+# include <string>
+# include <cstring>
+# include <list>
+# include <unistd.h>
+# include <fcntl.h>
+# include "Client.hpp"
+
+#define PORT "6667"
+
+class Server
+{
+
+	public:
+
+		Server(std::string pword, std::string arg_port);
+		Server( Server const & src );
+		~Server();
+
+		Server &		operator=( Server const & rhs );
+
+		void			set_listener_sock(); // set le socket listener
+		void			poll_loop();
+		void			polling();
+		void			handle_pfds();
+		void			handle_new_connection();
+		void			handle_command(std::list<pollfd>::iterator it);
+		void			add_socket_to_list(std::list<pollfd> *pfds, int filed, short ev, short rev);
+		std::string		reply(std::string reply_code, std::string target,std::string msg);
+		
+		/* accessors */
+		std::string		get_password();
+
+
+	private:
+
+		std::string			password;
+		std::string			port;
+		std::string			address;
+		std::list<pollfd>	pfds;
+		struct pollfd		*arr_pfds;
+		char				buf[1000];
+		int					listener;
+		std::list<Client>	clients;
+
+};
+
+std::ostream &			operator<<( std::ostream & o, Server const & i );
+
+#endif /* ********************************************************** SERVER_H */
