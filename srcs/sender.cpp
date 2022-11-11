a844df598a0889591bb75b47e3f8f8afe79f516f
Aamsi <ismaelouali45@gmail.com>
2022-11-11 17:05:25 +0100

    Send replies on connection

--- srcs/sender.cpp
+++ srcs/sender.cpp
@@ -6,9 +6,10 @@ void Server::send_to_client(Client client, std::string msg) {
 
 void Server::send_welcome_msg(Client client) {
     /* First welcome message to send to client when it connects to the server. */
-    std::string str_welcome = "Welcome to the Internet Relay Chat Network " + client.get_nick() + " !\n";
-    std::string str_host = "Your host " + this->address + " is runnning version 0.0.\n";
-    std::string str_date = "This sever has been started 20/10/2022 at 00:00:00.";
-    std::string reply = this->reply("001", client.get_nick(), str_welcome + str_host + str_date);
-    send(client.get_fd(), reply.c_str(), reply.length() + 1, SOCK_STREAM);
+    std::string str_welcome = ":" + this->address +  " 001 " + client.get_nick() +  " :Welcome to the Internet Relay Chat Network " + client.get_nick() + " !\r\n";
+    send_to_client(client, str_welcome);
+    std::string str_host = ":" + this->address +  " 002 " + client.get_nick() + " :Your host is " + this->address + ", runnning version 0.0.\r\n";
+    send_to_client(client, str_host);
+    std::string str_date = ":" + this->address +  " 003 " + client.get_nick() + " :This sever has been started 20/10/2022 at 00:00:00." + "\r\n";
+    send_to_client(client, str_date);
 }
