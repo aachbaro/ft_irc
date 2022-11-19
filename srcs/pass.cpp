3d92d0b6d1029d841c766c7a8c7e3de1f55b0b64
Ismael OUALI <iouali@e1r6p15.clusters.42paris.fr>
2022-11-19 13:44:42 +0100

    Fix segfault on pfd and client erase

--- srcs/pass.cpp
+++ srcs/pass.cpp
@@ -4,16 +4,19 @@ void    Server::reg_password(std::list<Client>::iterator client, std::string pas
 {
     std::string reply;
     if (client->isRegistered()) {
-        reply = ":" + address + " 462 " + client->get_nick() + " :You may not reregister";
+        reply = ":" + address + " 462 " + client->get_nick() + " :You may not reregister\r\n";
         send_to_client(*client, reply);
     }
     else if (pass != password) {
-        reply = ":" + address + " 464 " + client->get_nick() + " :Password incorrect";
+        reply = ":" + address + " 464 " + client->get_nick() + " :Password incorrect\r\n";
         send_to_client(*client, reply);
         for (std::list<pollfd>::iterator it_fds = pfds.begin(); it_fds != pfds.end(); ++it_fds) {
-        if (it_fds->fd == client->get_fd()) { pfds.erase(it_fds); break ; }
+            if (it_fds->fd == client->get_fd()) {
+                _pfdErase.push_back(it_fds);
+                break ;
+            }
         }
-        clients.erase(client);
+        _toErase.push_back(client);
     }
     else {
         client->set_pass(pass);
